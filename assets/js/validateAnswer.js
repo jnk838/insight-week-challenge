1ccfac66a973efc615c8985bf9806efd02bff3a1
Jacob Wilson <jacobswilsonglos@gmail.com>
2020-04-23 22:42:29 +0100

    Working version of answer checking

--- assets/js/validateAnswer.js
+++ assets/js/validateAnswer.js
@@ -1,3 +1,9 @@
+window.onload = function() {
+    document.getElementsByTagName("form")[0].onsubmit = function(e) {
+        e.preventDefault();
+    }
+};
+
 function checkAnswer(challenge, submission) {
     const HTTP = new XMLHttpRequest();
     const url = 'https://us-central1-insight-week-challenge.cloudfunctions.net/validate-answer';
@@ -7,13 +13,25 @@ function checkAnswer(challenge, submission) {
 
     HTTP.open("POST", url);
     HTTP.setRequestHeader("Content-Type", "application/json");
+    HTTP.onreadystatechange = function () {
+        if(HTTP.readyState === XMLHttpRequest.DONE) {
+          var status = HTTP.status;
+          if (status === 0 || (status >= 200 && status < 400)) {
+            // The request has been completed successfully
+            response = HTTP.responseText;
+
+            console.log(response);
+
+            if (response === "correct") {
+                alert("Well done! That is the correct answer!");
+            } else {
+                alert("Sorry, that isn't the right answer.");
+            }
+          } else {
+            // Oh no! There has been an error with the request!
+            console.log("Error with the response!");
+          }
+        }
+      };
     HTTP.send(json_up);
-
-    response = HTTP.responseText;
-
-    if (response === "correct") {
-        alert("Well done! That is the correct answer!");
-    } else {
-        alert("Sorry, that isn't the right answer.");
-    }
 }
