c305275521e29ee44cd4d9c486fe9ca920cefbf5
Jacob Wilson <jacobswilsonglos@gmail.com>
2020-04-23 18:33:35 +0100

    First attempt at answer vertification
    Doesn't actually work, as the JSON isn't being sent, but the logic in place all works

---
+++ assets/js/validateAnswer.js
@@ -0,0 +1,24 @@
+function checkAnswer(challenge, submission) {
+    const HTTP = new XMLHttpRequest();
+    const url = 'https://us-central1-insight-week-challenge.cloudfunctions.net/validate-answer';
+    const json_up = JSON.stringify({"chal": challenge, "sub": submission});
+
+    console.log(json_up);
+
+    HTTP.open("POST", url);
+    HTTP.setRequestHeader("Content-Type", "application/json");
+    HTTP.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log(HTTP.responseText);
+        }
+    };
+    HTTP.send(json_up);
+
+    response = HTTP.responseText;
+
+    if (response === "correct") {
+        alert("Well done! That is the correct answer!");
+    } else {
+        alert("Sorry, that isn't the right answer.");
+    }
+}
--- pages/challenges/welcome.md
+++ pages/challenges/welcome.md
@@ -10,3 +10,9 @@ I appear to have dropped my welcome message - its somehow been scrambled up...
 Here it is: `V0VMQ09NRV9UT19USEVfQ0hBTExFTkdFUw==`
 
 Please decode it for me, and then submit it on the submission platform.
+
+<form>
+    <label for="answer">Answer:</label><br>
+    <input type="text" id="submission" name="submission">
+    <input type="submit" value="Submit" onclick="javascript:checkAnswer('welcome', document.getElementById('submission').value)">
+</form>
